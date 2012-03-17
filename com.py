ec72a320197cae59460baadda5c1bc6d278f8c67
U-S-PC\S <S@S-PC.(none)>
2012-03-16 17:07:58 -0700

    updated to deal with appid properties

--- com.py
+++ com.py
@@ -6,8 +6,6 @@ from registry import Registry, HKEY_CLASSES_ROOT
 
 reg = Registry(HKEY_CLASSES_ROOT)
 
-#{03837511-098B-11D8-9414-505054503030}
-
 class Com:
 
     def __init__(self, clsid):
@@ -18,10 +16,9 @@ class Com:
                 self.name = value.content
             if value.name.lower() == "appid":
                 self.AppID = value.content
-                map( lambda k, v: self._setattr_(k, v),
-                     (val.name, val.value) for val in
-                     reg.get_subkey("AppID\\" + self.AppID))
-
+                map( lambda v: self._setattr_(v.name, v.content),
+                     ( val for val in
+                       reg.get_subkey("AppID\\" + self.AppID).get_subkeys()))
         for key in clsid_key.get_subkeys():
             if key.name.lower() == "interface":
                 self.interface = key.values()
@@ -30,17 +27,20 @@ class Com:
             if key.name.lower() == "programmable":
                 self.is_activex = True
             if key.name.lower() == "typelib":
-                self.typelib_guid = key.get_value()
-                typelib_key = reg.get_subkey("TypeLib\\" +
-                                             self.typelib_guid)
-                for ver in typelib_key.get_subkeys():
-                    GetModule(ver.get_subkey('0'))
-                        
-                        
-                        
-
-
-            
-        
-        #[k for k in get_subkeys("CLSID\\%s\InprocServer32" % z) ]
-    #get_values("CLSID\\%s\\TypeLib" % clsid)
+                self.typelib_guid = key.get_value().content
+                self.typelib_key = reg.get_subkey("TypeLib\\" +
+                                                  self.typelib_guid)
+                self.typelib_files = []
+                for ver in self.typelib_key.get_subkeys():
+                    for sv in ver.get_subkeys():
+                        if sv.num_keys:
+                            for arch in sv.get_subkeys():
+                                v = arch.get_value().content #fix this awefulness
+                                self.typelib_files.append(v)
+                                GetModule(v)
+                try:
+                    self.obj = CreateObject(self.clsid)
+                    self.methods = [ (name,args) for r,name,args,arg_names,n,n2
+                                     in self.obj._methods_ ]
+                except WindowsError:
+                    self.obj = "could not be instantiated"
